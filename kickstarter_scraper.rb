c06a3e06a2855ca498be51dc7344ffa45ced6949
Rosie Choi <rosiechoimanso@gmail.com>
2021-01-17 22:11:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,5 @@
 # require libraries/modules here
-
+require 'Nokogiri'
 def create_project_hash
 
 html = File.read('fixtures/kickstarter.html') # This just opens a file and reads it into a variable
